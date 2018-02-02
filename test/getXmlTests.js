496dd7f54c5e152007e37f47e320e56c9f6fe4ee
Jeri <jesomm@microsoft.com>
2018-02-02 11:35:06 -0800

    rename mock structure and move into test dir

--- test/getXmlTests.js
+++ test/getXmlTests.js
@@ -1,7 +1,19 @@
-var assert = require('assert');
+const assert = require('assert');
+const executeCommand = require('../executeCommand');
+const { newline, getAllCsprojUnderPath } = require('../getXml');
 
 describe('getXmlTests', () => {
     it('has a passing test', () => {
         assert.equal(true, true);
     });
 });
+
+describe('getAllCsprojUnderPath', () => {
+    it('', () => {
+        var path = executeCommand('cd').split(newline)[0];
+
+
+    });
+});
+
+
