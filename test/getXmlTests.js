34f1b5f0b74327e4a9ff6d0e0027b026d4116f7c
Jeri <jesomm@microsoft.com>
2018-02-02 16:19:42 -0800

    add a couple real passing tests

--- test/getXmlTests.js
+++ test/getXmlTests.js
@@ -1,21 +1,29 @@
-var chai = require('chai'), expect = chai.expect;
+var should = require('should');
 const executeCommand = require('../executeCommand');
 const { newline, getAllCsprojUnderPath } = require('../getXml');
 
 describe('getXmlTests', () => {
     it('has a passing test', () => {
-        var thing = true;
-        expect(thing).to.equal(true);
+        true.should.be.true;
     });
 });
 
 describe('getAllCsprojUnderPath', () => {
+    it('stops searching recursively in a file path once it finds the least-deep csproj', () => {
+        var path = executeCommand('cd').split(newline)[0];
+        path = `${path}\\test\\dir`;
+        var result = getAllCsprojUnderPath(path, false);
+
+        (result.indexOf('dir3.2.1.1_IGNORED.csproj') > -1).should.be.false;
+        (result.indexOf('dir3.2.1.csproj') > -1).should.be.true;
+    });
+
     it('gets all the things that matter and none of the things that do not', () => {
         var path = executeCommand('cd').split(newline)[0];
         path = `${path}\\test\\dir`;
-        var result = getAllCsprojUnderPath(path);
-        expect(result).to.equal('foo');
+        var result = getAllCsprojUnderPath(path, false);
+
+        (result.indexOf('IGNORE_ME.csproj') > -1).should.be.false;
+        (result.indexOf('dir3.2.2.csproj') > -1).should.be.true;
     });
 });
-
-
